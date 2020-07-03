AzureAfloat/signalk-server-node

fixed: crash when doing path based subscriptions because of meta entry

lib/deltacache.js
@@ -129,7 +129,13 @@ DeltaCache.prototype.getCachedDeltas = function (user, contextFilter, key) {
     }
     if (deltas) {
       // acc.push(_.reduce(deltas, ((delta, acc) => !acc ? delta : (new Date(delta.timestamp).getTime() > new Date(acc.timestamp).getTime() ? delta : acc))))
-      acc = acc.concat(_.values(deltas))
+      acc = acc.concat(
+        _.values(
+          _.pickBy(deltas, (val, key) => {
+            return key != 'meta'
+          })
+        )
+      )
     }
     return acc
   }, [])

wefleenor/signalk-server

src/deltacache.js
@@ -167,7 +167,13 @@ DeltaCache.prototype.getCachedDeltas = function(user, contextFilter, key) {
     }
     if (deltasToProcess) {
       // acc.push(_.reduce(deltas, ((delta, acc) => !acc ? delta : (new Date(delta.timestamp).getTime() > new Date(acc.timestamp).getTime() ? delta : acc))))
-      acc = acc.concat(_.values(deltasToProcess))
+      acc = acc.concat(
+        _.values(
+          _.pickBy(deltasToProcess, (val, key) => {
+            return key != 'meta'
+          })
+        )
+      )
     }
     return acc
   }, [])